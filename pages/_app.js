37f92411d13b88fe7c8f539db33d45aa10b61a8f
hiagoptk <hiagoptk@gmail.com>
2021-10-31 11:27:23 -0300

    Definindo a Home do site

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import '../pages/tarot/style.css'
+import "../components/HeaderPages/HeaderPage.css"
+import '../components/FooterPages/FooterPage.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
