cd8a16bdbed1f89c0f4cda8cd152886e86d6d34f
hiagoptk <hiagoptk@gmail.com>
2021-10-31 14:04:10 -0300

    editando o header

--- components/HeaderPages/HeaderPage.js
+++ components/HeaderPages/HeaderPage.js
@@ -1,6 +1,6 @@
-import {FaSun, FaBars, FaMoon, FaCode, FaPaintBrush, FaHandSparkles, FaJoint} from 'react-icons/fa'
+import {FaSun, FaBars, FaMoon, FaCode, FaPaintBrush, FaHandSparkles, FaJoint, FaHome, FaBrush, FaStar} from 'react-icons/fa'
 import {CgCardSpades} from 'react-icons/cg'
-
+import Link from 'next/link'
 
 function openNav() {
     let menu = document.getElementById("sideMenu");
@@ -26,16 +26,17 @@ const HeaderPage = () => {
     return (
         <navbar className="nav-black">
             <nav className="menu">
-                <h3>Hiago <FaSun/> Patrick</h3>
+                <Link href="/"><h3 className="title">Hiago <FaSun className="sun"/> Patrick</h3></Link>
                 <button id="menuBtn" className="show" onClick={openNav}><FaBars/></button>
             </nav>
             <div id="sideMenu" className="asideMenu">
                 <a href="javascript:void(0)" class="closebtn" onClick={closeNav}>&times;</a>
-                <a className="itemMenu">Artes <FaPaintBrush /></a>
-                <a className="itemMenu">Tarot <CgCardSpades /></a>
-                <a className="itemMenu">Reiki <FaHandSparkles /></a>
-                <a className="itemMenu">Bruxaria <FaMoon /></a>
-                <a className="itemMenu">Portifolio <FaCode /></a>
+                <p className="itemMenu"><Link href="/" > Home  </Link> <FaHome /></p>
+                <p className="itemMenu"><Link href="/tarot" > Tarot  </Link><CgCardSpades/></p>
+                <p className="itemMenu"><Link href="/" className="itemMenu">Artes </Link> <FaPaintBrush /> </p>
+                <p className="itemMenu"><Link href="/reiki" className="itemMenu">Reiki </Link> <FaHandSparkles /> </p>
+                <p className="itemMenu"><Link href="/bruxaria" className="itemMenu">Bruxaria </Link> <FaStar /> </p>
+                <p className="itemMenu"><Link href="/portifolio" className="itemMenu">Portifolio</Link> <FaCode /></p>
             </div>  
         </navbar>
     );
