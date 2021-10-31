f139a668a934b3fc208cdd48e0ef91ba59892f6b
hiagoptk <hiagoptk@gmail.com>
2021-10-31 11:28:09 -0300

    Construindo um Componente para o Footer

---
+++ components/FooterPages/FooterPage.js
@@ -0,0 +1,11 @@
+import {FaMoon} from 'react-icons/fa'
+
+const FooterPage = () => {
+    return (
+        <div className="footer">
+            <p>Made with Love by Hiago Patrick <FaMoon/></p>
+        </div>
+    );
+}
+
+export default FooterPage;
