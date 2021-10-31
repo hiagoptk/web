b3bde30592cfccaf5c941d2e2deedea1d245b4e5
hiagoptk <hiagoptk@gmail.com>
2021-10-31 16:10:41 -0300

    separando o menu em um componente próprio

--- components/HeaderPages/HeaderPage.js
+++ components/HeaderPages/HeaderPage.js
@@ -1,43 +1,18 @@
 import {FaSun, FaBars, FaMoon, FaCode, FaPaintBrush, FaHandSparkles, FaJoint, FaHome, FaBrush, FaStar} from 'react-icons/fa'
 import {CgCardSpades} from 'react-icons/cg'
 import Link from 'next/link'
+import SideMenu from '../SideMenu/SideMenu';
 
-function openNav() {
-    let menu = document.getElementById("sideMenu");
-        menu.classList.add("show");
 
-    let button = document.getElementById("menuBtn");
-        button.classList.add("hide")
-        button.classList.remove("show")
-
-  }
-  
-  /* Close/hide the sidenav */
-  function closeNav() {
-    let menu = document.getElementById("sideMenu");
-        menu.classList.remove("show"); 
-
-    let button = document.getElementById("menuBtn");
-        button.classList.add("show")
-        button.classList.remove("hide")
-  }
 
 const HeaderPage = () => {
     return (
         <navbar className="nav-black">
             <nav className="menu">
                 <Link href="/"><h3 className="title">Hiago <FaSun className="sun"/> Patrick</h3></Link>
-                <button id="menuBtn" className="show" onClick={openNav}><FaBars/></button>
+                <button id="menuBtn" className="show" ><FaBars/></button>
             </nav>
-            <div id="sideMenu" className="asideMenu">
-                <a href="" class="closebtn" onClick={closeNav}>&times;</a>
-                <p className="itemMenu"><Link href="/" > Home  </Link> <FaHome /></p>
-                <p className="itemMenu"><Link href="/tarot" > Tarot  </Link><CgCardSpades/></p>
-                <p className="itemMenu"><Link href="/" className="itemMenu">Artes </Link> <FaPaintBrush /> </p>
-                <p className="itemMenu"><Link href="/reiki" className="itemMenu">Reiki </Link> <FaHandSparkles /> </p>
-                <p className="itemMenu"><Link href="/bruxaria" className="itemMenu">Bruxaria </Link> <FaStar /> </p>
-                <p className="itemMenu"><Link href="/portifolio" className="itemMenu">Portifolio</Link> <FaCode /></p>
-            </div>  
+            <SideMenu id="aside" />
         </navbar>
     );
 };
